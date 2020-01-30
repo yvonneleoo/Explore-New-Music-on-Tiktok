b24460cda6c275ac093ac10e4fb7a7e4eb0081e3
yvonneleoo <43156350+yvonneleoo@users.noreply.github.com>
2020-01-30 14:10:38 -0500

    added the functionality to download data

--- ingestion/download_data.sh
+++ ingestion/download_data.sh
@@ -6,3 +6,9 @@ s3cmd put -vr fma_large s3://yvonneleoo/tiktok-music/;
 wget 'https://files.pushshift.io/tiktok/tiktok_25000000_sample.ndjson.zst';
 zstd -d tiktok_25000000_sample.ndjson.zst;
 s3cmd put -vr tiktok_25000000_sample.ndjson s3://yvonneleoo/tiktok-music/; 
+
+# download the fma meta data
+git clone 'https://github.com/mdeff/fma.git';
+cd ./fma;
+curl -O https://os.unil.cloud.switch.ch/fma/fma_metadata.zip;
+unzip -j fma_metadata.zip;

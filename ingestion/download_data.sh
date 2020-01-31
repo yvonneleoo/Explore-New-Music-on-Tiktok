e7954bb7b6b23b609e4262fa819cd7a31e0f25ed
yvonneleoo <43156350+yvonneleoo@users.noreply.github.com>
2020-01-31 18:25:29 -0500

    added the functionality to download data

--- ingestion/download_data.sh
+++ ingestion/download_data.sh
@@ -1,14 +1,15 @@
-# download the fma mudic data
+# download the fma mudic data to s3
 curl -O https://os.unil.cloud.switch.ch/fma/fma_large.zip;
 unzip fma_large.zip;
 s3cmd put -vr fma_metalarge s3://yvonneleoo/tiktok-music/; 
 
-# download the tiktok data
+# download the tiktok data to prosgresql directly
 wget 'https://files.pushshift.io/tiktok/tiktok_25000000_sample.ndjson.zst';
 zstd -d tiktok_25000000_sample.ndjson.zst;
-s3cmd put -vr tiktok_25000000_sample.ndjson s3://yvonneleoo/tiktok-music/; 
+psql -h ec2-34-197-195-174.compute-1.amazonaws.com -U postgres -d music_tiktok -c "CREATE TABLE tiktok(data varchar(1000))";
+cat tiktok_25000000_sample.ndjson | psql -h ec2-34-197-195-174.compute-1.amazonaws.com -U postgres -d music_tiktok -c "\COPY tiktok (data) FROM STDIN";
 
-# download the fma meta data
+# download the fma meta data to s3
 curl -O https://os.unil.cloud.switch.ch/fma/fma_metadata.zip;
 unzip fma_metadata.zip;
 s3cmd put -vr fma_metadata s3://yvonneleoo/tiktok-music/; 

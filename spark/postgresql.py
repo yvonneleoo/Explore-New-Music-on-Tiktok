1fa9f03d3d9caa14f61edb2566ffff16c31cc297
yvonneleoo <43156350+yvonneleoo@users.noreply.github.com>
2020-02-13 20:02:11 -0500

    Added functions to connect to postgres

--- spark/postgresql.py
+++ spark/postgresql.py
@@ -8,7 +8,7 @@ class PosgreConnector(object):
         self.db = 'music_tiktok'
         self.hostname = os.environ['POSTGRES_HN']
         self.url = 'jdbc:postgresql://{}:5432/{}'.format(self.hostname, self.db)
-        self.user = 'yvonneleoo'
+        self.user = os.environ['POSTGRES_USR']
         self.password = os.environ['POSTGRES_PWD']
         self.driver = 'org.postgresql.Driver'
         self.properties = {'user': self.user, 'password':self.password,'driver':self.driver} 

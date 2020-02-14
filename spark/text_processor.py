219b900428293ce8a7bffd7b7dfb02ca8b26d82b
yvonneleoo <43156350+yvonneleoo@users.noreply.github.com>
2020-02-14 00:11:42 -0500

    classes to process the text files

--- spark/text_processor.py
+++ spark/text_processor.py
@@ -8,6 +8,8 @@ from pyspark.sql import Window
 from pyspark.ml.feature import RegexTokenizer, StopWordsRemover, CountVectorizer, IDF
 from pyspark.ml import Pipeline
 
+import boto3
+
 class CleanTrackName(object):
    
     def __init__(self):
@@ -40,6 +42,106 @@ class CleanTrackName(object):
                  .withColumn('name_key_1', f.when(is_digit_udf(substring(col('song_title'),0,1)), f.lit('others')).otherwise(substring(col('song_title'),0,1)))
         return df1
 
+class CleanMusicInfo(CleanTrackName):
+
+	def __init__(self, spark):
+		super().__init__() 
+		self.client = boto3.client('s3')
+		self.spark = spark
+		self.s3_ = boto3.resource('s3')
+		self.bucketName = os.environ["S3_BUCKET"]
+		self.route =         
+
+	def read_csv_s3(self, route, df_name):
+    """
+    read csv from s3
+    """
+		obj_track = self.client.get_object(Bucket=self.bucketName, Key=route+df_name)
+		df = pd.read_csv(obj_track['Body'], header=[0, 1], skipinitialspace=True)
+		return df
+
+    def read_parquet_s3(self, df, route, partition=None, partition_key=None):
+    """
+    read parquet from s3
+    """
+        path = "s3a://{}/{}}/".format(self.bucketName, route)
+        if partition:
+        	path += '{}={}'.format(partition, partition_key)
+        return self.spark.read.load(path)
+        
+    def write_parquet_s3(self, df, route, partition=None):
+    """
+    write parquet to s3
+    """
+        path = "s3a://{}/{}}/".format(self.bucketName, route)
+        if partition:
+        	df.write\
+              .partitionBy(partition)\
+              .option("header","false")\
+              .parquet(path=path, mode="append") 
+        else:
+        	df.write\
+              .option("header","false")\
+              .parquet(path=path, mode="append")
+
+
+	def flatten_multi_index(self, df):
+	"""
+	flatten the multi index for the track.csv
+	"""
+		df.columns.set_levels(['track_id', 'album', 'artist', 'set', 'track'],level=0,inplace=True) # set the flatten column names
+        col_1 = df.columns.levels[1].tolist()
+        col_1[0] = 'track_id' # rename the index to track_id
+        df.columns.set_levels(col_1, level=1, inplace=True)
+        return df
+
+    def clean_sub_df(self, sub_df_name, sub_info_type=None): 
+    """
+    clean song info, artist info, and genres info
+    sub_df_name = [genre, track]
+    sub_info_type = [None, song, artist]
+    """
+    	if sub_df_name == 'genre':
+    		df = self.read_csv_s3('tiktok-music/fma_metadata/', 'genres.csv')
+    		return spark.createDataFrame(df.astype(str))
+    	elif sub_df_name == 'track':
+    		df = self.read_csv_s3('tiktok-music/fma_metadata/', 'track.csv')
+    		df1 = df.loc[:, df.columns.get_level_values(0).isin({'track_id',sub_info_type})]
+            df1.columns = df1.columns.droplevel()
+            df1 = spark.createDataFrame(df1.astype(str))
+            assert " " not in ''.join(df1.columns) 
+
+            if sub_info_type == 'song':
+            	df1 = df1.withColumn('genre_1', regexp_extract(col('genres'), '(\[)(\w+)(.+)', 2)) # flatten genre info and  capture the first genre
+                df1 = self.remove_noise(df1, 'title')
+
+            elif sub_info_type == 'artist':
+            	df1 = self.remove_noise(df1, 'name')
+
+            return df1
+
+    def merge_clean_info(self, track, artist, genres):
+    """
+    merge the track, artist and genres table to be a clean info table, for fast query in later steps;
+    set the first letter of song titles as a partition key, to partition the dataframe, to speed up title/artist similarity search in later steps;
+    """
+    	clean_info = track.join(artist,track.track_id == artist.track_id, how = 'inner')\ 
+                          .select(track['track_id'],track['title'].alias('song_title'),
+                                  artist['name'].alias("artist_name"),
+                                  track['genre_1'],track['original_title'].alias('original'))
+        clean_info = clean_info.join(genres, clean_info.genre_1 == genres.genre_id,how = 'left')\
+                               .select(clean_info['*'],genres['title'].alias('subgenre_name'),
+                                       genres['top_level']).sort(col('song_title'))
+
+        clean_info = clean_info.dropDuplicates(['song_title', 'artist_name']) # drop duplicate records if any 
+        print ("The total number of clean record is: {}".format(clean_info.count()))
+
+        clean_info = self.add_name_key(clean_info, which='music') # set the first letter of song titles as a partition key
+        
+        return clean_df      
+      
+      
+      
 class CalTextSimilarity(object):
 
     def __init__(self, sc):

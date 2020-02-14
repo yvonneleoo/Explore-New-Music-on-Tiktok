1001ab87e415dda8aa365c9512ffe6d38075cf18
yvonneleoo <43156350+yvonneleoo@users.noreply.github.com>
2020-02-13 20:03:15 -0500

    Added functions to connect to postgres

--- frontend/postgresql.py
+++ frontend/postgresql.py
@@ -5,17 +5,17 @@ from pyspark.sql.session import SparkSession
 class PosgreConnector(object):
 
     def __init__(self, sqlContext):
-        self.df = df
         self.db = 'music_tiktok'
-        self.url = 'jdbc:postgresql://ec2-34-197-195-174.compute-1.amazonaws.com:5432/%s' % self.db
-        self.user = 'yvonneleoo'
+        self.hostname = os.environ['POSTGRES_HN']
+        self.url = 'jdbc:postgresql://{}:5432/{}'.format(self.hostname, self.db)
+        self.user = os.environ['POSTGRES_USR']
         self.password = os.environ['POSTGRES_PWD']
         self.driver = 'org.postgresql.Driver'
         self.properties = {'user': self.user, 'password':self.password,'driver':self.driver} 
         self.sqlContext = sqlContext
 
     def read_from_db(self, table_name):
-        return self.sqlContext.read.jdbc(url=self.url, table='public.%s'%table_name, properties=self.properties)
+        return self.sqlContext.read.jdbc(url=self.url, table='public.{}'.format(table_name), properties=self.properties)
 
-    def write_to_db(self, df_clean, table_name, mode='append'):
-        df_clean.write.jdbc(url=self.url, table='public.%s'%table_name, mode=mode, properties=self.properties)
+    def write_to_db(self, df, table_name, mode='append'):
+        df.write.jdbc(url=self.url, table='public.{}'.format(table_name), mode=mode, properties=self.properties)
